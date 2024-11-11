72e2ef9cb95b0d90fe29c993b5b1eb6dfcc262c0
Александр <titov_s28@mail.ru>
2024-11-11 17:27:06 +0500

    add order

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+Order order = new Order(1,1,1,2000, "ewtg", "weqwe", "weq", "rqw", "В ожидании")
+
 app.MapGet("/", () => "Hello");
 
 app.Run();
